b49c1dc1ca683451ee8d27e9f3db373c4e119739
ryhoh <axis38akasira@gmail.com>
2021-04-11 01:33:19 +0900

    html and js divided

---
+++ static/main.js
@@ -0,0 +1,85 @@
+// Vue.js
+const vm = new Vue({
+    el: '#vue_app',
+    
+    data: () => ({
+        problem_list: null,
+        problem_list_errored: false,
+        problem_list_loding: false,
+
+        selected_problem: null,
+        tables: null,
+        expected_records: null,
+        expected_columns: null,
+        order_strict: null,
+        problem_errored: false,
+        problem_loding: false,
+        
+        sql: null,
+        judged: false,
+        result: null,
+        sql_submit_error: false,
+        sql_submitting: false,
+    }),
+
+    methods: {
+        submit_problem: function() {
+            this.result = 'Judging...';
+            this.sql_submitting = true;
+
+            const params = new URLSearchParams();
+            params.append('problem_name', this.selected_problem);
+            params.append('answer', this.sql);
+            axios
+                .post('/api/v1/submit', params)
+                .then(response => {
+                    this.result = response.data.Result;
+                })
+                .catch(error => {
+                    console.log(error.response);
+                    this.sql_submit_error = true;
+                })
+                .finally(() => {
+                    this.judged = true;
+                    this.sql_submitting = false;
+                });
+        },
+    },
+
+    watch: {
+        selected_problem: function(new_problem, old_problem) {
+            this.problem_loding = true;
+            axios
+                .get('/api/v1/problem', {
+                    params: {
+                        problem_name: new_problem,
+                    }
+                })
+                .then(response => {
+                    this.tables = response.data.tables;
+                    this.expected_records = response.data.expected_records;
+                    this.expected_columns = response.data.expected_columns;
+                    this.order_strict = response.data.order_strict;
+                })
+                .catch(error => {
+                    console.log(error.response);
+                    this.problem_errored = true;
+                })
+                .finally(() => this.problem_loding = false);
+        }
+    },
+
+    mounted() {
+        this.problem_list_loading = true;
+        axios
+            .get('/api/v1/problem_list')
+            .then(response => {
+                this.problem_list = response.data.problems;
+            })
+            .catch(error => {
+                console.log(error.response);
+                this.problem_list_errored = true;
+            })
+            .finally(() => this.problem_list_loading = false);
+    },
+});
