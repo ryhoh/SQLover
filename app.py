b1e02c64b699469af0d596a2768e8b500ae6fe5f
ryhoh <axis38akasira@gmail.com>
2021-04-10 22:45:04 +0900

    dev frontend

--- app.py
+++ app.py
@@ -1,42 +1,54 @@
+import glob
 import json
 import sqlite3
+from typing import Iterable
 
-import uvicorn
 from fastapi import FastAPI, Form, HTTPException
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
+import uvicorn
 
 import db
 import judge
 
 app = FastAPI()
 app.mount("/api/problems", StaticFiles(directory="problems"), name="problems")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
-explanation = 'SQL を練習できるサービスです。'
+explanation = 'Let\'s practice SQL on this service!'
 
 
 @app.get('/')
 async def root():
+    return FileResponse('static/index.html')
+
+
+@app.get('/api/v1/help')
+async def get_help():
     return {
         'help': explanation,
     }
 
 
-@app.get('/api/problems')
+@app.get('/api/v1/problem')
 def get_problem(problem_name: str):
-    check_problem_name(problem_name)
-    return FileResponse('problems/' + problem_name + '.json')
+    problem = load_problem(problem_name)
+
+    # JavaScript で読める形で正答の表を渡す
+    problem['expected_records'] = eval(problem["expected_expr"])
+    return problem
 
 
-@app.post('/api/submit')
-def submit_problem(problem_name: str = Form(...), answer: str = Form(...)):
-    check_problem_name(problem_name)
-    try:
-        with open("problems/" + problem_name + ".json", "r") as f:
-            problem = json.load(f)
-    except IOError:
-        raise HTTPException(status_code=503, detail="Problem load failed")
+@app.get('/api/v1/problem_list')
+def get_problem_list():
+    return {
+        'problems': problems_list()
+    }
 
+
+@app.post('/api/v1/submit')
+def submit_answer(problem_name: str = Form(...), answer: str = Form(...)):
+    problem = load_problem(problem_name)
     try:
         result = db.execute(ddl=problem["DDL"], query=answer)
     except sqlite3.OperationalError as e:
@@ -56,6 +68,22 @@ def submit_problem(problem_name: str = Form(...), answer: str = Form(...)):
 def check_problem_name(problem_name: str):
     if ".." in problem_name:  # 関係ないディレクトリにアクセスさせない
         raise HTTPException(status_code=403, detail="Forbidden")
+
+
+def load_problem(problem_name: str) -> dict:
+    check_problem_name(problem_name)
+    try:
+        with open("problems/" + problem_name + ".json", "r") as f:
+            problem = json.load(f)
+    except IOError:
+        raise HTTPException(status_code=503, detail="Problem load failed")
+    return problem
+
+
+def problems_list() -> Iterable[str]:
+    problems = glob.glob('problems/*.json', recursive=False)
+    return (name.removeprefix('problems/').removesuffix('.json')
+            for name in problems)
 
 
 if __name__ == '__main__':
